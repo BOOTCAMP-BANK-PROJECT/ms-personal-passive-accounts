7ea53bbe63bf209056f143835fe29f9dd30a1443
samuel14luis <luismendoza.samuel@gmail.com>
2022-05-03 11:50:49 -0500

    webclientservice working

--- src/main/java/com/bootcamp/personal/passiveaccounts/service/WebClientService.java
+++ src/main/java/com/bootcamp/personal/passiveaccounts/service/WebClientService.java
@@ -1,21 +1,26 @@
 package com.bootcamp.personal.passiveaccounts.service;
 
 import com.bootcamp.personal.passiveaccounts.MsPersonalPassiveAccountsApplication;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Service
+@Component
 public class WebClientService {
 
-    private final WebClient webClient;
+    private WebClient webClient;
+    private final WebClient.Builder webBuilder;
 
     public WebClientService(WebClient.Builder webBuilder) {
-        this.webClient = webBuilder
-                .baseUrl(MsPersonalPassiveAccountsApplication.getApiGateway())
-                .build();
+        this.webBuilder = webBuilder;
+        this.webClient = null;
     }
 
     public WebClient getWebClient() {
+        if(webClient == null) {
+            this.webClient = webBuilder
+                    .baseUrl(MsPersonalPassiveAccountsApplication.getApiGateway())
+                    .build();
+        }
         return webClient;
     }
 
